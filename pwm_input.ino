0fff3f5ab813574fa3a8fc1d0432015ae7ca9fc5
Brian Ho <brian@codeforart.com>
2020-03-30 11:45:40 -0700

    Cleaning up of variable names

--- pwm_input.ino
+++ pwm_input.ino
@@ -1,25 +1,25 @@
 #define DEBUG
 
-#define CH_PIN_1 3
-#define CH_PIN_2 4
-#define CH_PIN_3 5
-#define CH_PIN_4 6
+#define CH_1_PIN 3
+#define CH_2_PIN 4
+#define CH_3_PIN 5
+#define CH_4_PIN 6
 #define INPUT_TIMEOUT 40000 // in microsecond
 
 // Global Variables
-int CH_1 = 0;
-int CH_2 = 0;
-int CH_3 = 0;
-int CH_4 = 0;
+int intCh1 = 0;
+int intCh2 = 0;
+int intCh3 = 0;
+int intCh4 = 0;
 
 char data[128];
 
 void setup() {
   // 4 pwm inputs and output
-  pinMode(CH_PIN_1, INPUT);
-  pinMode(CH_PIN_2, INPUT);
-  pinMode(CH_PIN_3, INPUT);
-  pinMode(CH_PIN_4, INPUT);
+  pinMode(CH_1_PIN, INPUT);
+  pinMode(CH_2_PIN, INPUT);
+  pinMode(CH_3_PIN, INPUT);
+  pinMode(CH_4_PIN, INPUT);
 
   Serial.begin(115200);
   sprintf(data, "CH1 CH2 CH3 CH4");
@@ -28,19 +28,19 @@ void setup() {
 
 int pulseInAndMap(byte ch) {
     // TODO: might use shift or mask to reduce precision
-    return pulseIn(ch, HIGH, INPUT_TIMEOUT)/10;
+    return pulseIn(ch, HIGH, INPUT_TIMEOUT) / 10;
 }
 
 void readChannelInput() {
-  CH_1 = pulseInAndMap(CH_PIN_1);
-  CH_2 = pulseInAndMap(CH_PIN_2);
-  CH_3 = pulseInAndMap(CH_PIN_3);
-  CH_4 = pulseInAndMap(CH_PIN_4);
+  intCh1 = pulseInAndMap(CH_1_PIN);
+  intCh2 = pulseInAndMap(CH_2_PIN);
+  intCh3 = pulseInAndMap(CH_3_PIN);
+  intCh4 = pulseInAndMap(CH_4_PIN);
 }
 
 void loop() {
     readChannelInput();
 
-    sprintf(data, "%d %d %d %d", CH_1, CH_2, CH_3, CH_4);
+    sprintf(data, "%d %d %d %d", intCh1, intCh2, intCh3, intCh4);
     Serial.println(data);
 }
