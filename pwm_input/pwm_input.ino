0e3e52212f3e3516a9aa367e858d34d59c8af318
Brian Ho <brian@codeforart.com>
2020-04-07 12:03:22 -0700

    Refactor the code to be ready for single stick operation

--- pwm_input/pwm_input.ino
+++ pwm_input/pwm_input.ino
@@ -6,12 +6,16 @@
 #define CH_2_PIN 11
 #define CH_3_PIN 12
 #define CH_4_PIN 13
+#define PWM_INPUT_MIN 1090
+#define PWM_INPUT_MAX 1890
 #define INPUT_TIMEOUT 40000 // in microsecond
 
 #define TRACK_LEFT_FORW 3
 #define TRACK_LEFT_BACK 4
 #define TRACK_RIGHT_FORW 7
 #define TRACK_RIGHT_BACK 8
+#define SERVO_PWM_MIN -256
+#define SERVO_PWM_MAX  256
 
 #define enA 5
 #define enB 6
@@ -38,19 +42,18 @@ void setup() {
     Serial.begin(115200);
 }
 
-int runTrack(byte PIN, int input, byte TRACK_A, byte TRACK_B) {
-    int pwm = map(input, 1090, 1890, -256, 256);
+int runTrack(byte PIN, int pwm, byte TRACK_A, byte TRACK_B) {
 
-    if (pwm > 60) {
+    if (pwm > 64) {
         digitalWrite(TRACK_A, LOW);
         digitalWrite(TRACK_B, HIGH);
-    } else if (pwm < -60) {
+    } else if (pwm < -64) {
        digitalWrite(TRACK_A, HIGH);
        digitalWrite(TRACK_B, LOW);
     } else  {
       digitalWrite(TRACK_A, HIGH);
       digitalWrite(TRACK_B, HIGH);
-   }
+    }
 
     pwm = abs(pwm);
     analogWrite(PIN, pwm);
@@ -60,8 +63,12 @@ int runTrack(byte PIN, int input, byte TRACK_A, byte TRACK_B) {
 
 void loop() {
 
-    int left = runTrack(enA, ch1.read(), TRACK_LEFT_FORW, TRACK_LEFT_BACK);
-    int right = runTrack(enB, ch2.read(), TRACK_RIGHT_FORW, TRACK_RIGHT_BACK);
+    int left = map(ch1.read(), PWM_INPUT_MIN, PWM_INPUT_MAX, SERVO_PWM_MIN, SERVO_PWM_MAX);
+    int right = map(ch2.read(), PWM_INPUT_MIN, PWM_INPUT_MAX, SERVO_PWM_MIN, SERVO_PWM_MAX);
+
+    // base on mode can make it go through a transform
+    left = runTrack(enA, left, TRACK_LEFT_FORW, TRACK_LEFT_BACK);
+    right = runTrack(enB, right, TRACK_RIGHT_FORW, TRACK_RIGHT_BACK);
 
     sprintf(data, "%d %d", left, right);
     Serial.println(data);
