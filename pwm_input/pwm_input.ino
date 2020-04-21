011c447deff7d90b36b92a31272f57e1f9f2b4b8
Brian Ho <brian@codeforart.com>
2020-04-20 19:56:36 -0700

    Re-implemented the steering control
    While moving forward or backward, the steering is used to compensate the track speed.
    While in stationary, the steering will spin the tracks in opposite direction.

--- pwm_input/pwm_input.ino
+++ pwm_input/pwm_input.ino
@@ -5,40 +5,39 @@
 #define ROTATION 0.7071067812
 #define THRESHOLD 64
 
-#define CH_1_PIN 2
-#define CH_2_PIN 3
-#define CH_3_PIN 10
+#define CH_1_PIN 10
+#define CH_2_PIN 11
+#define CH_3_PIN 12
 
-#define PWM_INPUT_MIN 1100
-#define PWM_INPUT_MID 1500
-#define PWM_INPUT_MAX 1900
+#define PWM_INPUT_MIN 1080
+#define PWM_INPUT_MID 1480
+#define PWM_INPUT_MAX 1880
 #define INPUT_TIMEOUT 40000 // in microsecond
 
-#define TRACK_LEFT_FORW 4
-#define TRACK_LEFT_BACK 5
-#define enA 6
+#define TRACK_LEFT_FORW 3
+#define TRACK_LEFT_BACK 4
+#define enA 5
+#define enB 6
 #define TRACK_RIGHT_FORW 7
 #define TRACK_RIGHT_BACK 8
-#define enB 9
 
-#define STICK_PWM_MIN -360
-#define STICK_PWM_MAX 360
+#define STICK_PWM_MIN -250
+#define STICK_PWM_MAX 250
 
-#define SERVO_PWM_MIN -255
-#define SERVO_PWM_MAX 255
+#define SERVO_PWM_MIN -250
+#define SERVO_PWM_MAX 250
 
 char data[128];
 
-// 70-95-120 25 apart
-Channel pwmCh1(CH_1_PIN);
-Channel pwmCh2(CH_2_PIN);
+Channel Ch1(CH_1_PIN);
+Channel Ch2(CH_2_PIN);
 // Channel pwmCh3(CH_3_PIN);
 
 void setup()
 {
 
-  // pwmCh1.begin(true);
-  // pwmCh2.begin(true);
+  // Ch1.begin(true);
+  // Ch2.begin(true);
   // pwmCh3.begin(true);
   delay(5000);
 
@@ -67,11 +66,6 @@ void runTrack(byte PIN, int pwm, byte TRACK_A, byte TRACK_B)
     digitalWrite(TRACK_A, HIGH);
     digitalWrite(TRACK_B, LOW);
   }
-  else
-  {
-    digitalWrite(TRACK_A, HIGH);
-    digitalWrite(TRACK_B, HIGH);
-  }
 
   analogWrite(PIN, abs(pwm));
 }
@@ -86,33 +80,57 @@ bool inputOutsideBound(int left, int right)
 
 void loop()
 {
-  int left, right, x, y;
+  int steer = 0, throttle = 0, left = 0, right = 0;
 
-  int ch1 = pwmCh1.getValue();
-  int ch2 = pwmCh2.getValue();
+  int ch1 = Ch1.getValue();
+  int ch2 = Ch2.getValue();
 
-#ifdef SINGLE_STICK
-
-  left = map(ch1, PWM_INPUT_MIN, PWM_INPUT_MAX, STICK_PWM_MIN, STICK_PWM_MAX);
-  right = map(ch2, PWM_INPUT_MIN, PWM_INPUT_MAX, STICK_PWM_MIN, STICK_PWM_MAX);
-
-  if (right < -THRESHOLD)
-    left = -left;
-
-  x = left * ROTATION + right * ROTATION;
-  y = left * -ROTATION + right * ROTATION;
-
-  left = constrain(x, SERVO_PWM_MIN, SERVO_PWM_MAX);
-  right = constrain(y, SERVO_PWM_MIN, SERVO_PWM_MAX);
-#endif
-
-  if (!inputOutsideBound(left, right))
+  if (ch1 == 0 || ch2 == 0)
   {
-    // base on mode can make it go through a transform
-    runTrack(enA, left, TRACK_LEFT_FORW, TRACK_LEFT_BACK);
-    runTrack(enB, right, TRACK_RIGHT_FORW, TRACK_RIGHT_BACK);
+    delay(1000);
   }
+  else
+  {
 
-  //  sprintf(data, "%d %d %d %d %d %d", ch1, ch2, left, right, x, y);
-  //  Serial.println(data);
+    steer = map(ch1, PWM_INPUT_MIN, PWM_INPUT_MAX, STICK_PWM_MIN, STICK_PWM_MAX);
+    throttle = map(ch2, PWM_INPUT_MIN, PWM_INPUT_MAX, STICK_PWM_MIN, STICK_PWM_MAX);
+
+    if (throttle > -THRESHOLD && throttle < THRESHOLD)
+    {
+      left = steer;
+      right = -steer;
+    }
+    else
+    {
+      if (throttle < -THRESHOLD)
+      {
+        steer = -steer;
+      }
+      // apply the steer on throttle
+      left = throttle;
+      right = throttle;
+
+      if (steer < -THRESHOLD)
+      {
+        right = throttle - abs(steer);
+        if (right < 0) right = 0;
+      }
+      else if (steer > THRESHOLD)
+      {
+        left = (throttle - abs(steer);
+        if (left < 0) left = 0;
+      }
+    }
+
+    left = constrain(left, SERVO_PWM_MIN, SERVO_PWM_MAX);
+    right = constrain(right, SERVO_PWM_MIN, SERVO_PWM_MAX);
+
+    {
+      // base on mode can make it go through a transform
+      runTrack(enA, left, TRACK_LEFT_FORW, TRACK_LEFT_BACK);
+      runTrack(enB, right, TRACK_RIGHT_FORW, TRACK_RIGHT_BACK);
+    }
+  }
+  sprintf(data, "%d %d %d %d %d %d", ch1, ch2, steer, throttle, left, right);
+  Serial.println(data);
 }
